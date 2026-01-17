85874f6df27880e1221ee24fd5c23dddf6ebf7f1
Raja Kalavala <45046033+RajaKalavala@users.noreply.github.com>
2026-01-17 21:10:34 +0530

    create - Add shell and mfe dynamic apps

---
+++ apps/shell/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { NxWelcome } from './nx-welcome';
+import { Route } from '@angular/router';
+import { loadRemote } from '@module-federation/enhanced/runtime';
+
+export const appRoutes: Route[] = [
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      loadRemote<typeof import('dashboard/Routes')>('dashboard/Routes').then(
+        (m) => m!.remoteRoutes,
+      ),
+  },
+  {
+    path: '',
+    component: NxWelcome,
+  },
+];
